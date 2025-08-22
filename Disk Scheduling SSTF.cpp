dbee3325a1690816dc56e0b68d329b2dc7fd2189
Nusrat Ahmmed Maisha <nusratamaisha@gmail.com>
2025-08-22 22:27:30 +0600

    Create Disk Scheduling SSTF.cpp

---
+++ Disk Scheduling SSTF.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, head;
+    cin>>n>>head;
+
+    vector<int> requests(n);
+    for (int i = 0; i < n; i++) {
+        cin>>requests[i];
+    }
+  
+    vector<bool> visited(n, false);
+    int totalSeek = 0;
+    int currentHead = head;
+
+    for (int i = 0; i < n; i++) {
+        int idx=-1;
+        int minDist=INT_MAX;
+
+        // Find the closest unvisited request
+        for (int j = 0; j < n; j++) {
+            if (!visited[j]) {
+                int distance=abs(currentHead - requests[j]);
+                if (distance<minDist) {
+                    minDist=distance;
+                    idx=j;
+                }
+            }
+        }
+
+        visited[idx]=true;
+        totalSeek+=minDist;
+        currentHead=requests[idx];
+    }
+
+    cout<<totalSeek<<endl;
+}
